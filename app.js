e141913c674c2880f08894246695054c307e00b1
alexho <alexho0205@gmail.com>
2023-06-06 16:43:35 +0800

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let engine = require('ejs-locals');
+let app = express();
+app.engine('ejs', engine);
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.get('/', function (req, res) {
+    res.render('index', {'title': '首頁',});
+})
+let port = 3000;
+app.listen(port);
