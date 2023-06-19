b5cbb43e575ce823c03b92377b4f3c41f5320d4b
alexho <alexho0205@gmail.com>
2023-06-19 16:45:13 +0800

    add format1 layout

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ app.engine('ejs', engine);
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+
+app.get('/format1',function(req,res){
+    res.render('format1', {'title': '',});
+})
+
 app.get('/', function (req, res) {
     res.render('index', {'title': '首頁',});
 })
+
 let port = 3000;
 app.listen(port);
---
+++ public/js/format1.js
@@ -0,0 +1,194 @@
+
+// 建制(中間)畫布區
+var topCanvas = new fabric.Canvas('topCanvas', { preserveObjectStacking: true, width: 400, height: 200 });
+var middleCanvas = new fabric.Canvas('middleCanvas', { preserveObjectStacking: true, width: 400, height: 200 });
+var bottomCanvas = new fabric.Canvas('bottomCanvas', { preserveObjectStacking: true, width: 400, height: 200 });
+// 畫布中所有的物件
+var itexts = [];
+
+
+function addTextToWB(e) {
+   let itext = new fabric.IText('新增一段文字', { fontSize: 45, fontFamily: '' });
+   WB.add(itext);
+}
+
+function loadTextAttr(itext) {
+   if (itext.fontFamily) {
+      $('#fontFamilyOpt').val(itext.fontFamily);
+   } else {
+      $('#fontFamilyOpt').val('default');
+   }
+   if (itext.fontSize) {
+      $('#fontSizeOpt').val(itext.fontSize);
+   }
+}
+
+function setTextSize(e) {
+   const fontSize = $('#fontSizeOpt').find(":selected").val();
+   setStyle('fontSize', fontSize);
+}
+
+function setTextFontFamily(e) {
+   const fontFamily = $('#fontFamilyOpt').find(":selected").val();
+   setStyle('fontFamily', fontFamily);
+}
+
+function setTextColor(e) {
+   const fontColor = e.target.getAttribute('zcolor');
+   setStyle('fill', fontColor);
+}
+
+// 變更當前 itext 的 style
+function setStyle(styleName, sytleValue) {
+   var obj = WB.getActiveObject();
+   if (obj === null) {
+      return;
+   }
+   obj.enterEditing();
+   obj[styleName] = sytleValue;
+   obj.dirty = true;
+   obj.exitEditing();
+   console.log('set style:', styleName, sytleValue);
+}
+
+
+function exportJPG() {
+   WB.setBackgroundColor('#FFFFFF', WB.renderAll.bind(WB));
+   var dataURL = WB.toDataURL({ format: 'png', quality: 1 });
+   var link = document.createElement('a');
+   link.href = dataURL;
+   link.download = 'canvas.png';
+   link.click();
+}
+
+function exportPDF() {
+   let width = 400;
+   let height = 500;
+
+   //let pdf = new jsPDF('l', 'px', [width, height]);
+   let pdf = new jsPDF('p', 'px', [width, height]);
+   let cav = document.getElementById('white_board');
+
+   //then we get the dimensions from the 'pdf' file itselfd
+   width = pdf.internal.pageSize.getWidth();
+   height = pdf.internal.pageSize.getHeight();
+   pdf.addImage(cav, 'PNG', 0, 0, width, height);
+   pdf.save("download.pdf");
+
+}
+
+var text1, text2, text3 = null;
+function initText() {
+   text1 = new fabric.IText('新增一段文字', { fontSize: 45, fontFamily: 'default' });
+   topCanvas.add(text1);
+   text2 = new fabric.IText('新增一段文字', { fontSize: 45, fontFamily: 'default', editable: false });
+   middleCanvas.add(text2);
+   text3 = new fabric.IText('新增一段文字', { fontSize: 45, fontFamily: 'default', editable: false });
+   bottomCanvas.add(text3);
+}
+
+function chageTxt1(e) {
+   text1.text = $(this).val();
+   topCanvas.renderAll();
+}
+
+function chageTxt2(e) {
+   text2.text = $(this).val();
+   middleCanvas.renderAll();
+}
+
+function chageTxt3(e) {
+   text3.text = $(this).val();
+   bottomCanvas.renderAll();
+}
+
+function setTextFontFamily1(e) {
+   const fontFamily = $('#fontStyle1').find(":selected").val();
+   setStyle(text1, 'fontFamily', fontFamily);
+}
+
+function setTextFontFamily2(e) {
+   const fontFamily = $('#fontStyle2').find(":selected").val();
+   setStyle(text2, 'fontFamily', fontFamily);
+}
+
+function setTextFontFamily3(e) {
+   const fontFamily = $('#fontStyle3').find(":selected").val();
+   setStyle(text3, 'fontFamily', fontFamily);
+}
+
+function setTextFontSize1(e) {
+   const fontSize = $('#fontSize1').find(":selected").val();
+   setStyle(text1, 'fontSize', fontSize);
+}
+
+function setTextFontSize2(e) {
+   const fontSize = $('#fontSize2').find(":selected").val();
+   setStyle(text2, 'fontSize', fontSize);
+}
+
+function setTextFontSize3(e) {
+   const fontSize = $('#fontSize3').find(":selected").val();
+   setStyle(text3, 'fontSize', fontSize);
+}
+
+function setTextFontColor1(e) {
+   const fontColor = $('#fontColor1').find(":selected").val();
+   setStyle(text1, 'fontColor', fontColor);
+}
+
+function setTextFontColor2(e) {
+   const fontColor = $('#fontColor2').find(":selected").val();
+   setStyle(text2, 'fontColor', fontColor);
+}
+
+function setTextFontColor3(e) {
+   const fontColor = $('#fontColor3').find(":selected").val();
+   setStyle(text3, 'fontColor', fontColor);
+}
+
+function setStyle(itext, styleName, styleValue) {
+
+   //itext.fontFamily = sytleValue;
+   if (styleName === 'fontFamily') {
+      itext.fontFamily = styleValue;
+   } else if (styleName === 'fontSize') {
+      itext.fontSize = styleValue;
+   } else if (styleName === 'fontColor') {
+      itext.fill = styleValue;
+   }
+   itext.dirty = true;
+   topCanvas.renderAll();
+   middleCanvas.renderAll();
+   bottomCanvas.renderAll();
+   console.log('set style:', styleName, styleValue);
+}
+
+// add image to background
+function addImageToGB(e) {
+   let imageUrl = e.target.src;
+   $('.drawImage').css('background-image', 'url(' + imageUrl + ')');
+
+}
+
+$(document).ready(function () {
+   initText();
+
+   $('#txtArea1').on("change keyup", chageTxt1);
+   $('#txtArea2').on("change keyup", chageTxt2);
+   $('#txtArea3').on("change keyup", chageTxt3);
+   $('#fontStyle1').on('change', setTextFontFamily1);
+   $('#fontStyle2').on('change', setTextFontFamily2);
+   $('#fontStyle3').on('change', setTextFontFamily3);
+   $('#fontSize1').on('change', setTextFontSize1);
+   $('#fontSize2').on('change', setTextFontSize2);
+   $('#fontSize3').on('change', setTextFontSize3);
+   $('#fontColor1').on('change', setTextFontColor1);
+   $('#fontColor2').on('change', setTextFontColor2);
+   $('#fontColor3').on('change', setTextFontColor3);
+   $('.memeimage').on('click', addImageToGB);
+
+
+
+
+});
